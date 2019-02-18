a4afa2685d9d06fec218aebe9925eb29208cdc0c
jeroenvollenbrock <jeroen@vollenbrock.nl>
2019-02-18 17:54:30 +0100

    Fix mood remote Fix pairing test highlights

--- drivers/JSJSLW104WH/device.js
+++ drivers/JSJSLW104WH/device.js
@@ -6,4 +6,19 @@ const Remote = require('../../lib/devices/remote');
 // module.exports = RFDevice => class 50970RDevice extends MyGenericDevice(RFDevice) {
 // and define MyGenericDevice like so
 // module.exports = RFDevice => class MyGenericDevice extends RFDevice {
-module.exports = RFDevice => class JSJSLW104WHDevice extends Remote(RFDevice) {};
+module.exports = RFDevice => class JSJSLW104WHDevice extends Remote(RFDevice) {
+
+    async onFlowTriggerFrameReceived(args, state) {
+
+        const [key, cmd] = (args.button || '').split('_');
+
+        switch(key) {
+            case 'mood':
+                return state.channel === 4 && state.unit === 4 && state.command === 2 && state.parameter === (256 << parseInt(cmd));
+            case 'power':
+                return state.channel === 4 && state.unit === 4 && state.command === 0 && state.parameter === 192;
+            default:
+                return state.channel === 1 && state.unit === Number(key) && state.onoff === (cmd === 'on') && state.parameter === 0;
+        }
+    }
+};
